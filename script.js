359f30542550c3bde26d6f5cbd45ba66c04a2a49
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-18 09:37:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const checkboxes = document.querySelectorAll(".toggle");
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", () => {
+            const checkedCheckboxes = document.querySelectorAll(".toggle:checked");
+
+            if (checkedCheckboxes.length > 2) {
+                checkedCheckboxes[0].checked = false;
+            }
+        });
+    });
+});
